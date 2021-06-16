555b5d8521554dbde9b10ec031473bc8e9b48f4f
wangyufeng <1037338981@qq.com>
2021-06-16 14:52:37 +0800

    王宇峰新建了两句话

--- demo01/src/HelloWorld.java
+++ demo01/src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("王宇峰");
+        System.out.println("王宇峰1");
+        System.out.println("王宇峰2");
     }
 }
